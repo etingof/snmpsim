aa0b8d8548868e9dd0c5551b73bcfb5efa9f6902
elie <elie>
2013-03-30 17:58:15 +0000

    directory sequence made mutable to ease runtime modification

--- snmpsim/confdir.py
+++ snmpsim/confdir.py
@@ -3,33 +3,33 @@ import sys
 import tempfile
 
 if sys.platform[:3] == 'win':
-  variation = (
+  variation = [
     os.path.join(os.environ['HOMEPATH'], 'SNMP Simulator', 'Variation'),
     os.path.join(os.environ['APPDATA'], 'SNMP Simulator', 'Variation'),
     os.path.join(os.environ['PROGRAMFILES'], 'SNMP Simulator', 'Variation')
-  )
-  data = (
+  ]
+  data = [
     os.path.join(os.environ['HOMEPATH'], 'SNMP Simulator', 'Data'),
     os.path.join(os.environ['APPDATA'], 'SNMP Simulator', 'Data'),
     os.path.join(os.environ['PROGRAMFILES'], 'SNMP Simulator', 'Data')
-  )
+  ]
 elif sys.platform == 'darwin':
-  variation = (
+  variation = [
     os.path.join(os.environ['HOME'], '.snmpsim', 'variation'),
     os.path.join('/', 'usr', 'local', 'share', 'snmpsim', 'variation')
-  )
-  data = (
+  ]
+  data = [
     os.path.join(os.environ['HOME'], '.snmpsim', 'data'),
     os.path.join('/', 'usr', 'local', 'share', 'snmpsim', 'data')
-  )
+  ]
 else:
-  variation = (
+  variation = [
     os.path.join(os.environ['HOME'], '.snmpsim', 'variation'),
     os.path.join(sys.prefix, 'share', 'snmpsim', 'variation')
-  )
-  data = (
+  ]
+  data = [
     os.path.join(os.environ['HOME'], '.snmpsim', 'data'),
     os.path.join(sys.prefix, 'share', 'snmpsim', 'data')
-  )
+  ]
 
 cache = os.path.join(tempfile.gettempdir(), 'snmpsim')
