6983a49817dd035ceab39daa89cea29c63a8e166
elie <elie>
2013-04-19 14:16:46 +0000

    report expanded arguments

--- variation/subprocess.py
+++ variation/subprocess.py
@@ -18,20 +18,23 @@ def init(snmpEngine, **context):
         moduleOptions['shell'] = int(moduleOptions['shell'])
  
 def variate(oid, tag, value, **context):
-    log.msg('subprocess: executing external process "%s"\r\n' % value)
+    args = [ x\
+             .replace('@DATAFILE@', context['dataFile'])\
+             .replace('@OID@', str(oid))\
+             .replace('@TAG@', tag)\
+             .replace('@ORIGOID@', str(context['origOid']))\
+             .replace('@ORIGTAG@', str(sum([ x for x in context['origValue'].tagSet[0]])))\
+             .replace('@ORIGVALUE@', str(context['origValue']))\
+             .replace('@SETFLAG@', str(int(context['setFlag'])))\
+             .replace('@NEXTFLAG@', str(int(context['nextFlag'])))\
+             .replace('@SUBTREEFLAG@', str(int(context['subtreeFlag'])))\
+             for x in value.split(' ') ]
+
+    log.msg('subprocess: executing external process "%s"\r\n' % ' '.join(args))
+
     try:
         return oid, tag, subprocess.check_output(
-            [ x\
-              .replace('@DATAFILE@', context['dataFile'])\
-              .replace('@OID@', str(oid))\
-              .replace('@TAG@', tag)\
-              .replace('@ORIGOID@', str(context['origOid']))\
-              .replace('@ORIGTAG@', str(sum([ x for x in context['origValue'].tagSet[0]])))\
-              .replace('@ORIGVALUE@', str(context['origValue']))\
-              .replace('@SETFLAG@', str(int(context['setFlag'])))\
-              .replace('@NEXTFLAG@', str(int(context['nextFlag'])))\
-              .replace('@SUBTREEFLAG@', str(int(context['subtreeFlag'])))\
-              for x in value.split(' ') ], shell=moduleOptions['shell']
+            args, shell=moduleOptions['shell']
         )
     except subprocess.CalledProcessError:
         log.msg('subprocess: external program execution failed\r\n')
