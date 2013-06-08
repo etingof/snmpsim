483b741c2f39a7b3474ac975598bd38a35357408
elie <elie>
2013-06-08 10:52:17 +0000

    automatic file rotation implemented

--- snmpsim/log.py
+++ snmpsim/log.py
@@ -35,18 +35,71 @@ class FileLogger(AbstractLogger):
         if not priv:
             raise error.SnmpsimError('Bad log file params, need filename')
         try:
-            self._fileobj = open(priv[0], 'a')
+            self._fileobj, self._file = open(priv[0], 'a'), priv[0]
         except:
             raise error.SnmpsimError(
-                'Log file open failure: %s' % sys.exc_info()[1]
+                'Log file %s open failure: %s' % (priv[0], sys.exc_info()[1])
             )
+        self._maxsize = 0
+        self._maxage = 0
+        self._lastrotate = 0
+        if len(priv) > 1 and priv[1]:
+            localtime = time.localtime()
+            if priv[1][-1] in ('k', 'K'):
+                self._maxsize = int(priv[1][:-1]) * 1024
+            elif priv[1][-1] in ('m', 'M'):
+                self._maxsize = int(priv[1][:-1]) * 1024 * 1024
+            elif priv[1][-1] in ('g', 'G'):
+                self._maxsize = int(priv[1][:-1]) * 1024 * 1024 * 1024
+            elif priv[1][-1] in ('h', 'H'):
+                self._maxage = int(priv[1][:-1]) * 3600
+                self._lastrotate = time.mktime(localtime[:4]+(0,0)+localtime[6:]) % self._maxage
+            elif priv[1][-1] in ('d', 'D'):
+                self._maxage = int(priv[1][:-1]) * 3600 * 24
+                self._lastrotate = time.mktime(localtime[:3]+(0,0,0)+localtime[6:]) % self._maxage
+            else:
+                raise error.SnmpsimError(
+                    'Unknown log rotation criteria %s, use K,M,G for size and H,M for time limits' % priv[1]
+                )
 
-    def timestamp(self):
+            self._infomsg = 'Log file %s, rotation rules are: age: %s mins, size %sKB' % (self._file, self._maxage/60, self._maxsize/1024)
+            self(self._infomsg)
+
+    def timestamp(self, now):
         return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + \
-               '.%.3d' % int((time.time() % 1) * 1000)
+               '.%.3d' % ((now % 1) * 1000,)
 
     def __call__(self, s):
-        self._fileobj.write('%s %s[%s]: %s\n' % (self.timestamp(), self.progId, getattr(os, 'getpid', lambda x: 0)(), s))
+        now = time.time()
+        if self._maxsize:
+            try:
+                size = os.stat(self._file)[6]
+            except:
+                size = 0
+        if self._maxsize and size >= self._maxsize or \
+                self._maxage and (now % self._maxage) < self._lastrotate:
+            newName = self._file + '.%d%.2d%.2d%.2d%.2d' % time.localtime()[:5]
+            if not os.path.exists(newName):
+                self._fileobj.close()
+                try:
+                    os.rename(self._file, newName)
+                except:
+                    pass  # losing log
+                else:
+                    try: 
+                        self._fileobj = open(self._file, 'a')
+                    except:
+                        pass # losing log
+                    else:
+                        self(self._infomsg)
+                        if self._maxage:
+                            self._lastrotate = int(now) % self._maxage
+
+        try:
+            self._fileobj.write('%s %s[%s]: %s\n' % (self.timestamp(now), self.progId, getattr(os, 'getpid', lambda x: 0)(), s))
+        except IOError:
+            pass # losing log
+
         self._fileobj.flush()
 
 class ProtoStdLogger(FileLogger):
