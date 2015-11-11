096a2dc37b091c0a4b2fb12bf74662b2a45f9e7e
elie <elie>
2015-11-11 21:52:31 +0000

    pidfile fix

--- snmpsim/daemon.py
+++ snmpsim/daemon.py
@@ -51,7 +51,7 @@ else:
 
         try:
             fd, nm = tempfile.mkstemp(dir=os.path.dirname(pidfile))
-            os.write(fd, '%d\n' % os.getpid())
+            os.write(fd, ('%d\n' % os.getpid()).encode('utf-8'))
             os.close(fd)
             os.rename(nm, pidfile)
         except:
