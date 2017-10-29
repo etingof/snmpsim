b5436964ee8d96308af1c1850ef49630f2cd7a2a
Ilya Etingof <etingof@gmail.com>
2017-10-29 21:35:48 +0100

    0.4.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,7 @@ html_theme = 'alabaster'
 #
 html_theme_options = {
     'logo': 'logo.svg',
-    'description': '<p align=center><i>Brought by SNMP Labs</i></p>',
+    'description': '<p align=left><i><b>Brewing free software for the greater good</i></b></p>',
     'show_powered_by': False,
     'github_user': 'etingof',
     'github_repo': 'snmpsim',
